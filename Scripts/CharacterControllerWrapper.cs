35f47170a7609ea146547d0256d50c845277fe46
Daniel Walls <walls.dk@gmail.com>
2023-08-13 21:22:25 -0700

    initial commit

---
+++ Scripts/CharacterControllerWrapper.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+namespace poetools.Core.Abstraction.Unity
+{
+    [RequireComponent(typeof(CharacterController))]
+    public class CharacterControllerWrapper : PhysicsComponent
+    {
+        private CharacterController _character;
+        
+        private void Awake()
+        {
+            _character = GetComponent<CharacterController>();
+        }
+
+        private void Update()
+        {
+            _character.Move(Velocity * Time.deltaTime);
+
+            if (Mathf.Round(_character.velocity.sqrMagnitude) < Mathf.Round(Velocity.sqrMagnitude))
+                Velocity = _character.velocity;
+        }
+        
+        public override Vector3 Velocity { get; set; }
+    }
+}
---
+++ Scripts/PhysicsComponent.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+namespace poetools.Core.Abstraction
+{
+    public interface IPhysicsComponent
+    {
+        Vector3 Velocity { get; set; }
+    }
+
+    public abstract class PhysicsComponent : MonoBehaviour, IPhysicsComponent
+    {
+        public abstract Vector3 Velocity { get; set; }
+    }
+}

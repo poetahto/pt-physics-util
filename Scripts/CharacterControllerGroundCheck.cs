f032c8eee444ec2086855edb92dc733aa2778b97
Daniel Walls <walls.dk@gmail.com>
2023-08-18 22:02:30 -0700

    temp character ground check, gravity, improvements to wrapper

---
+++ Scripts/CharacterControllerGroundCheck.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using pt_player_3d.Scripts;
+using UnityEngine;
+
+namespace DefaultNamespace
+{
+    [RequireComponent(typeof(CharacterController))]
+    public class CharacterControllerGroundCheck : GroundCheck3d
+    {
+        private CharacterController _characterController;
+
+        private void Awake()
+        {
+            _characterController = GetComponent<CharacterController>();
+        }
+
+        public override bool IsGrounded => _characterController.isGrounded;
+
+        public override bool TryGetGround(out GroundData3d groundData3d)
+        {
+            groundData3d = default;
+            return false;
+        }
+    }
+}

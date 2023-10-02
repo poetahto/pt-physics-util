0ef82ba8ac43a1ebd1358bcbb11550d505d70267
Daniel Walls <walls.dk@gmail.com>
2023-10-01 17:35:11 -0700

    some useful extensions for working with colliders

---
+++ Scripts/PhysicsExtensions.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+namespace Core
+{
+    public static class PhysicsExtensions
+    {
+        public static bool TryGetComponentWithRigidbody<T>(this Collider collider, out T value)
+        {
+            bool hasRigidbody = collider.attachedRigidbody != null;
+
+            if (hasRigidbody && collider.attachedRigidbody.TryGetComponent(out value))
+                return true;
+
+            else if (collider.TryGetComponent(out value))
+                return true;
+
+            return false;
+        }
+
+        public static GameObject GetRootGameObject(this Collider collider)
+        {
+            return collider.attachedRigidbody == null
+                ? collider.gameObject
+                : collider.attachedRigidbody.gameObject;
+        }
+    }
+}

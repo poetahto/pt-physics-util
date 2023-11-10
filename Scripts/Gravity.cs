25a2bab290cfb6b3037328110e8b5c8d11cd4494
poetahto <walls.dk@gmail.com>
2023-11-09 16:39:10 -0800

    make gravity steppable, needs more work though

--- Scripts/Gravity.cs
+++ Scripts/Gravity.cs
@@ -3,34 +3,43 @@ using UnityEngine;
 
 namespace poetools.Core.Abstraction
 {
+    // todo: needs cleaning
+    
     [RequireComponent(typeof(IPhysicsComponent))]
     public class Gravity : MonoBehaviour
     {
+        [SerializeField]
         public Vector3 downDirection = Vector3.down;
+        
+        [SerializeField]
         public float amount = -Physics.gravity.y;
-        [SerializeField]private float idleGravity;
-        [SerializeField] public GroundCheck3d groundCheck;
+        
+        [SerializeField]
+        private float idleGravity;
+        
+        [SerializeField]
+        public GroundCheck3d groundCheck;
+        
+        [SerializeField] 
+        private bool autoUpdate = true;
 
         private IPhysicsComponent _physicsComponent;
 
         private void Awake()
         {
             _physicsComponent = GetComponent<IPhysicsComponent>();
-            // debug = DebugWhiteboard.Instance.AddLabel(() =>
-                // $"velocity: {_physicsComponent.Velocity}\ngrounded: {_physicsComponent.IsGrounded}");
         }
-
-        // private IDisposable debug;
-
-        // private void OnDestroy()
-        // {
-            // debug.Dispose();
-        // }
 
         private void FixedUpdate()
         {
+            if (autoUpdate)
+                Tick(Time.fixedDeltaTime);
+        }
+
+        public void Tick(float deltaTime)
+        {
             _physicsComponent.Velocity = !groundCheck.IsGrounded || _physicsComponent.Velocity.y > 0
-                ? _physicsComponent.Velocity + downDirection * (amount * Time.deltaTime)
+                ? _physicsComponent.Velocity + downDirection * (amount * deltaTime)
                 : _physicsComponent.Velocity + Vector3.down * idleGravity;
         }
     }
